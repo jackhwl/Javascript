90b1bd3c31b7783ea85affd14da54457d4ef35f8
Wenlin Huang <jackhwl@hotmail.com>
2023-02-09 17:10:15 -0500

    Demo: Adding Module Examples

--- ModulesInJavascript/index.js
+++ ModulesInJavascript/index.js
@@ -1,28 +1,5 @@
+import { calculateTax } from "./calculateTax.js"
 import { roundMoney } from "./roundMoney.js"
-
-// Tax Rates
-const lowRate = .05
-const highRate = .095
-
-// Calculates taxes based on price
-function calculateTax(totalPrice) {
-  let taxRate = 0
-
-  if (totalPrice >= 10 && totalPrice < 100) {
-    taxRate = lowRate
-  } else {
-    taxRate = highRate
-  }
-
-  const preTax = roundMoney(totalPrice)
-  const postTax = roundMoney((preTax * taxRate) + preTax)
-
-  return {
-    preTax,
-    postTax,
-    taxRate
-  }
-}
 
 // Create ten examples and use the calc function
 for (let x = 0; x < 10; ++x) {
